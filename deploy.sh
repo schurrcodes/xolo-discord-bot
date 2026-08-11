1c6a77d1cbae7058fe1c4baf8c6f66feb896e372
xolotlin <mendezamaru734@gmail.com>
2026-08-11 13:50:53 -0700

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
 set -e
 
 echo "Starting deployment..."
 
-# Pull latest changes from github
+# Pull latest updates from GitHub
 git pull origin main
 
-# Activate virual environment & update dependencies
+# Activate virtual environment & update packages
 source .venv/bin/activate
 pip install -r requirements.txt
 
-# Restart PM2 service process
-pm2 restart discord-bot
+# Restart if process exists, otherwise start it for the first time
+echo "Reloading Discord bot in PM2..."
+pm2 restart discord-bot || pm2 start .venv/bin/python --name "discord-bot" -- main.py
 
-echo "Deployment Completed."
+# Save PM2 state
+pm2 save
+
+echo "✅ Deployment complete!"
